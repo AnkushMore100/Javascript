3a04564f363cbb613b51f69e455b195e63be4d65
Your Name <you@example.com>
2023-11-20 20:18:33 +0530

    Intro js and variables

---
+++ JAVA/variables.js
@@ -0,0 +1,22 @@
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
+
+// Automatically (var-Default)
+// Using var
+// Using let
+// Using const
+
+// let/var/const vN = value;
+
+let marks = 50;
+
+marks1 = 90;
+
+let name ; // declaration
+
+console.log(name);
+
+name = "Ankush"
+console.log(name);
+
+let SirName = 'More' // decleration + def
