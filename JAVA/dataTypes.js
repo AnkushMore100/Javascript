5503e02a226a6462fb067fb233af00c6528550e7
Your Name <you@example.com>
2023-11-21 22:23:14 +0530

    data type

---
+++ JAVA/dataTypes.js
@@ -0,0 +1,16 @@
+// data types: 1. Primitive Data type 2. Nonpremitive
+// Thre premitive data types: Single value holder -- in javascipt include number,str,
+
+
+
+//  number: whole no or decimal no
+let amount = 98.9898;  //989898
+
+// typeof(): is used to get data type of data
+
+console.log(typeof(amount));
+
+// string datat type : ''/""
+
+let city ="Mumbai is a big city 545421";
+console.log(typeof(city));
