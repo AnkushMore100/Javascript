b05cc5f992cce8cc767432c82c11cd53fddb64dd
Your Name <you@example.com>
2023-12-08 20:29:05 +0530

    if else else if

---
+++ JAVA/condition.js
@@ -0,0 +1,32 @@
+
+
+var marks = 60;
+
+// If
+if(marks >75 ){
+console.log("Distinction");
+}
+
+
+// if else
+
+if (marks >35){
+    console.log("Pass......");
+}else{
+    console.log("fail");
+}
+
+// else if : multiple
+
+if(marks >= 75){
+    console.log("distinction")
+} else if(marks >= 60 && marks < 75){
+    console.log("First Class")
+} else if(marks >= 50 && marks < 60){
+    console.log("Second Class")
+}else if(marks >= 35 && marks < 50){
+    console.log("Passed")
+}
+ else{
+    console.log("Failed")
+}
