25348ec62b1f4947b7261946f75be5c0c4b7776c
Your Name <you@example.com>
2023-12-09 11:22:39 +0530

    switch operator

--- JAVA/condition.js
+++ JAVA/condition.js
@@ -19,18 +19,48 @@ if (marks >35){
 // else if : multiple
 
 if(marks >= 75){
-    console.log("distinction")
+    console.log("distinction");
 } else if(marks >= 60 && marks < 75){
-    console.log("First Class")
+    console.log("First Class");
 } else if(marks >= 50 && marks < 60){
-    console.log("Second Class")
+    console.log("Second Class");
 }else if(marks >= 35 && marks < 50){
-    console.log("Passed")
+    console.log("Passed");
 }
  else{
-    console.log("Failed")
+    console.log("Failed");
 }
 
 // else if(marks > 0 && marks <35){
 //     console.log("Fail!!!")
 // }  as pn chalt pn else lavava lagto
+
+
+// switch  vlue direct match krto
+// 1.what is diff bten if eles and switch
+
+// 2. is break keyword manditory?
+// ==> No but switch behivour is change so its break required
+
+// default keyword also not required write in switch
+
+let amount = 100;
+switch(amount){
+    case 50:
+         console.log("You have 50 Rs");
+        break;
+    case 60:
+        console.log("You have 60 Rs");
+        break;
+    case 100:
+        console.log("You have 100 Rs");
+        break;
+    case 90:
+        console.log("You have 90 Rs");
+        break;
+
+    default:
+        console.log("You  have Money")
+        break;
+
+}
