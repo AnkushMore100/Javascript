a0caa3d895164140f65b70404efd02cd507d6845
Your Name <you@example.com>
2023-12-09 11:22:16 +0530

    logical operator

---
+++ JAVA/logical operator.js
@@ -0,0 +1,77 @@
+// assignment operator
+
+let a =90;
+
+// equality operator
+
+b =80;
+
+if(b == 80 ){ //equality operator : it will check only value not data type
+    console.log("b has 80")
+}
+
+if (b == "80"){//==== equality operator : it chks for value as well as data tye
+   console.log("sbjfnksd")
+
+}
+
+// greaterthan operator
+
+if (b > 20){
+    console.log("B is greater than 20");
+}
+
+if (b < 20){
+    console.log("jkjkjk");
+}
+
+if (b == 20){
+    console.log("B is equal to 20");
+}
+
+if (b >= 20){
+    console.log("b is greater than equal to 20");
+}
+
+if (b <= 20){
+    console.log("b is less than equal to 20");
+}
+
+if (b != 20){
+    console.log("b is not equal to 20");
+}
+
+let c = true; // True asel tr compiler aat mdhe janar
+if(c){
+    console.log("sjhksd")
+}
+
+let d = false; // false asel tr compiler aat mdhe janar nhi else part run
+if(c){
+    console.log("sjhksd")
+}
+
+if(!c){ // false >> true
+console.log("!!!")
+}
+
+let e =90;
+if(e){
+    console.log('eee')
+}
+
+let k =90;
+if(!k){  //false
+    console.log('kk')
+}
+
+
+let f = null;
+if(f){ // True
+    console.log('eee')
+}
+
+let j = null;
+if(!j){  // True
+    console.log('jjj')
+}
