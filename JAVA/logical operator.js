43f723ae8d873fba463a137c9e9cfd75c2e64a79
Your Name <you@example.com>
2023-12-10 11:16:54 +0530

    logical And Or

--- JAVA/logical operator.js
+++ JAVA/logical operator.js
@@ -75,3 +75,56 @@ let j = null;
 if(!j){  // True
     console.log('jjj')
 }
+
+// Logical &&(and) and logical |(or)
+
+// T && T >> T; F && T, T && F -->> F
+
+var marks = 60;
+
+if(marks >75 ){
+console.log("Distinction");
+}
+
+
+
+
+if (marks >35){
+    console.log("Pass......");
+}else{
+    console.log("fail");
+}
+
+
+if(marks >= 75){
+    console.log("distinction");
+} else if(marks >= 60 && marks < 75){
+    console.log("First Class");
+} else if(marks >= 50 && marks < 60){
+    console.log("Second Class");
+}else if(marks >= 35 && marks < 50){
+    console.log("Passed");
+}
+ else{
+    console.log("Failed");
+}
+
+let mark = 50;
+if( mark == 50 && mark > 35 && mark > 45){
+    console.log("pass")
+}
+
+// T||F,F||T --> T ;F ||F --> F
+
+let x =50;
+let y =20;
+if(x == 50 || y!=20 || y > 10 ){
+    console.log("jgkj")
+}
+
+// interview point of view
+let age =35;
+if(age >=18 && age<=60){
+    console.log("Valid EMp");
+};
+
