8a331405e6537e3299c3844022d1587ce9dc9c30
Your Name <you@example.com>
2023-12-14 20:20:28 +0530

    for  loops

---
+++ JAVA/loops.js
@@ -0,0 +1,47 @@
+// for(let i =1; i<11; i++){
+//     console.log(i)
+// };
+
+// for(s1;ConvolverNode;inc/dcr){
+//     //
+// }
+
+
+
+
+// let x = 10;
+// // x = x + 1
+// x++;   // ++ increment oprator
+
+// // x = x-1 
+// x--; // decrement operator
+// console.log(x);
+
+// for(let a = 1; a < 11 ; a++){
+//     console.log(a);
+// };
+
+
+// empty loop infinite mdhe jato
+
+
+let color = ['red','blue','black','pink'];
+let color1 =[];
+for(let r = 0; r < color.length; r++){
+      if(color[r]!='blue'){
+        color1.push(color[r])
+      }
+}
+console.log(color1);
+
+
+
+// let color2 = ['red','blue','black','pink'];
+// let color3 =[];
+// for(let r = 0; r < color2.length; r++){
+//       if(color2[r]=='blue'){
+//           continue
+//         color3.push(color2[r])
+//         console.log(color3);
+//       }
+// }
