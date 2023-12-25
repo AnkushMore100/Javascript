47a11155204a350fa1f585773788b16b2b0e2404
Your Name <you@example.com>
2023-12-25 19:48:22 +0530

    Data Binding

--- Angular Project/myApplication/src/app/app.component.ts
+++ Angular Project/myApplication/src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApplication';
+
+
+  //propertyName : data type = value;
+
+  a = 20;
+  b: number = 90;
+  c!: number;
+  city : string = "pune";
+  //1. one way data binding 2. two way data binding
+  //1.one way data binding
+  //1.string interpolation >>{{}} >> syntax {{propertyName}}
+  //2.Property binding >> [] >> syntax [attribute] = "PropertyName"
+  //3. event binding>>()>>on event will bind data to the property
+  //Two way data bind >> [()]>> [(ngModel)]="propertyName"
+
+  myName ="ankush"
+  isDisable =false;
+
+
+ fruit! : string;
+
+
+  constructor(){ }
+
+  submit(){
+   this.fruit = 'Mango';
+  } 
 }
+
+
+
