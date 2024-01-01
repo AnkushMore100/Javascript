f9533fa4e58f824b55ffb83205a99efc502ddd00
Your Name <you@example.com>
2024-01-01 20:07:52 +0530

    ng if directive

---
+++ Angular Project/myApplication/src/app/directive/directive.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive',
+  templateUrl: './directive.component.html',
+  styleUrls: ['./directive.component.css']
+})
+export class DirectiveComponent {
+showDiv : boolean = true;
+toggle(){
+// this.showDiv = false;
+this.showDiv = !this.showDiv;
+}
+}
